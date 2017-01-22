7ff3dd7ea1a6bf66c45b879d24b9bdebf8d5a730
Joe Spedale <jspedale@gmail.com>
2017-01-22 11:36:47 -0500

    Added sidebar scaffolding.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Sidebar from './Sidebar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="main-container">
+          <Sidebar />
+          <div className="content"></div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div className="sidebar">
+        <p>MY RECIPE BOX</p>
+      </div>
+    )
+  }
+}
+
+
+export default Sidebar;
