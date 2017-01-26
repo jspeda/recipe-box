d401d83b1535e3c3251bae3afe1986a3f8e3e56e
Joe Spedale <jspedale@gmail.com>
2017-01-26 08:30:59 -0500

    Added contentEditable tags to recipecards. Changes not yet hooked up to state.

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -7,12 +7,13 @@ class RecipeCard extends Component {
 
     return (
       <div className="recipe-card">
-        <div className="recipe-title"><div className="title-text">{details.title}</div>
+        <div className="recipe-title">
+          <div className="title-text" contentEditable="true">{details.title}</div>
           <div className="edit-delete"><span><button onClick={this.props.openModal}>edit</button></span>
           <span><button onClick={() => this.props.removeRecipe(this.props.index)}>delete</button></span></div>
         </div>
-        <div className="recipe-contents-container">
-          <ul className="recipe-ingredients">
+        <div className="recipe-contents-container" contentEditable="true">
+          <ul className="recipe-ingredients" contentEditable="true">
             {
             details.ingredients
               .split(';')
