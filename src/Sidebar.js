0b2400c5ad4e51b5140d6c0eb96b1ad74cd9828f
Joe Spedale <jspedale@gmail.com>
2017-02-01 23:53:15 -0500

    Added live search to sidebar.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 
 class Sidebar extends Component {
+  constructor() {
+    super();
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.state = {
+      search: ''
+    };
+  }
+
+  handleSearchChange(searchTerm) {
+    this.setState({ search: searchTerm });
+    console.log(this.state.search);
+  }
+
+
   render() {
     const { recipes } = this.props;
     return (
       <div className="sidebar">
         <h1>MY RECIPE BOX</h1>
-        <input placeholder="search" />
+        <input
+          placeholder="search"
+          onChange={(e) => this.handleSearchChange(e.target.value)}
+          value={this.state.search}
+         />
         <p>RECIPE LIST</p>
         <div className="sidebar-button-list">
           <ul>
           {
             Object
               .keys(recipes)
+              .filter((key) => `${recipes[key].title}`.toUpperCase()
+              .indexOf(this.state.search.toUpperCase()) >= 0)
               .map(key => <li key={key}>
                 <div className="sidebar-button-container">
                 <button
