ef8082afb397bfb914267c65ee44d0c7c6ccb238
Joe Spedale <jspedale@gmail.com>
2017-02-06 18:45:25 -0500

    Installed react-router-dom and established basic splash page for signing in.

---
+++ src/Splash.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Splash extends Component {
+  render() {
+    return (
+      <div>
+        <h1>WELCOME TO RECIPE BOX</h1>
+        <h2>please sign in</h2>
+
+      </div>
+    )
+  }
+}
+
+
+export default Splash;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
+import Splash from './Splash';
 import './index.css';
 
+
+const Root = () => {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Splash} />
+          <Route exact path="/:userId" component={App} />
+        </div>
+      </Router>
+    )
+}
+
+
 ReactDOM.render(
-  <App />,
+  <Root />,
   document.getElementById('root')
 );
